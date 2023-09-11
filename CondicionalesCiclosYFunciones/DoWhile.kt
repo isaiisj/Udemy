0e5bf73cf65e866eb638d3ad5ac6ea0ae6449bc7
isaiisj <47548422+isaiisj@users.noreply.github.com>
2023-09-11 02:07:46 -0600

    Create DoWhile.kt

---
+++ CondicionalesCiclosYFunciones/DoWhile.kt
@@ -0,0 +1,26 @@
+fun main(args: Array<String>) {
+  /*
+   for(x in 1..10){
+  
+    println(x)
+  
+   }*/
+  
+   //Introducción a ciclos.
+   //while - do while
+  
+  var x = 18
+  println("Ciclo While")
+  while (x <= 10){
+   println(x)
+   x++
+  }
+  
+  var y = 18
+   println("Ciclo Do While")
+   do {
+   println(y)
+   y++
+  }while(y <= 10)
+
+}
