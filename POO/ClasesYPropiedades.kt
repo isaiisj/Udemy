ac5207982328de2858d658c36cd8399aa49323c7
isaiisj <47548422+isaiisj@users.noreply.github.com>
2023-09-11 02:28:06 -0600

    Create ClasesYPropiedades.kt

---
+++ POO/ClasesYPropiedades.kt
@@ -0,0 +1,13 @@
+class Felino {
+
+
+   //Propiedades de la clase
+   var tipoFelino:String = ""
+   var cantidadOjos:Int = 2
+   var cantidadPatas:Int = 4
+   var colorPelo:String = "Gris"
+   var peso:Double = 1.8
+
+
+
+}
