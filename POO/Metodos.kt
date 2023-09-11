46fe2730c58a72c84107b2a18765d30242646cae
isaiisj <47548422+isaiisj@users.noreply.github.com>
2023-09-11 02:25:37 -0600

    Create Metodos.kt

---
+++ POO/Metodos.kt
@@ -0,0 +1,24 @@
+class Felino {
+
+   //Propiedades de la clase
+   var tipoFelino:String = ""
+   var cantidadOjos:Int = 2
+   var cantidadPatas:Int = 4
+   var colorPelo:String = "Gris"
+   var peso:Double = 1.8
+
+  //Metodos de la clase
+
+   fun comer() {
+      println("El felino esta comiendo")
+   }
+
+   fun correr() {
+      println("El felino esta corriendo")
+   }
+
+   fun cazar() {
+      println("El felino esta cazando")
+   }
+
+}
