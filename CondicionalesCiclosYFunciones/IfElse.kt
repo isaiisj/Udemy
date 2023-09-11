20301433184d6e21ce984fe95cf02e3396499187
isaiisj <47548422+isaiisj@users.noreply.github.com>
2023-09-11 01:47:18 -0600

    Create IfElse.kt

---
+++ CondicionalesCiclosYFunciones/IfElse.kt
@@ -0,0 +1,27 @@
+fun main(args: Array<String>) {
+
+ //Introducción condicionales.
+
+ // if
+
+var billetera:Int = 400
+var precioPs5 = 300
+var precioNintendoGameCube = 990
+
+      //true
+ if (billetera >= precioNintendoGameCube){
+
+  print("Si me puedo comprar un GameCube")
+
+ }else{
+
+  print("No me puedo comprar un GameCube")
+  if(billetera >= precioPs5){
+   print(" Si me puedo comprar un ps5")
+  }else{
+
+   print("Tengo que ahorrar mas")
+
+  }
+
+ }
