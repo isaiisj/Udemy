3132480508abaa67eb2c9a4c12a47d5ec26dec91
isaiisj <47548422+isaiisj@users.noreply.github.com>
2023-09-10 20:47:56 -0600

    Create TiposDatosYAsignacionExplicita.kt

---
+++ VariablesArreglosYOperadores/TiposDatosYAsignacionExplicita.kt
@@ -0,0 +1,36 @@
+fun main(args: Array<String>) {
+  var leccion = "Tipos de datos, asignacion implicita y explicita"
+
+    //Int
+    var x:Int = 18
+
+    //Flotantes Double
+
+    var y:Double = 15.18
+
+    y = Double.MAX_VALUE
+
+    println(y)
+
+
+    // Character Char
+
+    var z:Char = 'a'
+
+    // String Cadena de Texto
+
+    var a:String = "Hola Mundo"
+
+    //Bool Boolean Boleanos
+
+    var soyMayorEdad:Boolean = true
+
+
+
+    println(leccion)
+    println(x)
+    println(z)
+    println(a)
+    println(soyMayorEdad)
+
+}
