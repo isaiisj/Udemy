a2a2debd602381083cfacd26d8d47a450a24a5a2
isaiisj <47548422+isaiisj@users.noreply.github.com>
2023-09-10 20:27:07 -0600

    Create TiposDatosYAsignacionExplicita.kt

---
+++ TiposDatosYAsignacionExplicita.kt
@@ -0,0 +1,37 @@
+import java.awt.geom.QuadCurve2D
+
+fun main(args: Array<String>) {
+
+    var leccion = "Tipos de datos, asignacion implicita y explicita"
+
+    //Int
+    var x:Int = 18
+
+    //Flotantes Double
+
+    var y:Double = 15.18
+
+    y = Double.MAX_VALUE
+
+    println(y)
+
+
+    // Character Char
+
+    var z:Char = 'a'
+
+    // String Cadena de Texto
+
+    var a:String = "Hola Mundo"
+
+    //Bool Boolean Boleanos
+
+    var soyMayorEdad:Boolean = true
+
+
+
+    println(leccion)
+
+
+
+}
