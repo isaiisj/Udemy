d5fccd06f295f6274c8b10d3601288c7c20c1cf5
isaiisj <47548422+isaiisj@users.noreply.github.com>
2023-09-10 20:58:14 -0600

    Create Operadores.kt

---
+++ VariablesArreglosYOperadores/Operadores.kt
@@ -0,0 +1,27 @@
+fun main(args: Array<String>) {
+  //Introducción a Operadores Matematicos, de comparación y lógicos.
+
+ // + - / *
+
+ var numeroUno = 10
+ var numeroDos = 2
+ var resultado =  numeroUno / numeroDos
+
+// <  >  <=   >=  ==
+// ! && ||
+// Not - And - Or
+ val a = 15
+ val b = 15                     //false ||  True
+val resultadoBoleano:Boolean = ((a > b) || (b == a))
+  print(resultadoBoleano)
+
+
+ /*
+ a) a > 3                                    b) a > b                                               c) a < c
+ d) c < b                                    e) b != a                                               f) a == 8
+ g) b * c == 18                              h) a * b == -30                                    i) c / b < a
+ j) c / b == -11                             k) c / b == -4                                       l) a + c + b == 5
+ m) (a+b == 8) && (a-b == 2)                 n) (a+b == 8) || (a-b == 6)
+ o) a > 3 && b > 4 && c < 3                  p) a > 3 && b >= 3 && c < -3
+ */
+}
