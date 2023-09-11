8a2864c8386a106cba478a2b11ee93255b41d7ab
isaiisj <47548422+isaiisj@users.noreply.github.com>
2023-09-10 20:55:09 -0600

    Create Arreglos.kt

---
+++ VariablesArreglosYOperadores/Arreglos.kt
@@ -0,0 +1,11 @@
+fun main(args: Array<String>) {
+  var arregloEnteros = arrayOf(1,2,3)
+
+   var arregloStrings = arrayOf("Hola","Mundo","Cruel")
+
+   var arregloBoleanos = arrayOf(true,false,true)
+
+    print(arregloEnteros[2])
+    println(arregloStrings[2])
+    print(arregloBoleanos[2])
+}
