01fe0c65480a42bc980558dc53c8ac0314100f03
isaiisj <47548422+isaiisj@users.noreply.github.com>
2023-09-10 20:46:49 -0600

    Create VariablesConstantes.kt

---
+++ VariablesArreglosYOperadores/VariablesConstantes.kt
@@ -0,0 +1,15 @@
+fun main(args: Array<String>) {
+
+    var bienvenida = "Hola mundo cruel"
+
+    val fechaCumpleanos = "1 Enero de 1989"
+
+    //1
+    println(bienvenida)
+    //2
+    println("" + fechaCumpleanos)
+    //3
+    println("Program arguments: ${bienvenida} y esto es un extra")
+
+
+}
