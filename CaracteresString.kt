2654756169363395e556455b1acda7c414c2fba7
isaiisj <47548422+isaiisj@users.noreply.github.com>
2023-09-10 20:36:08 -0600

    Create CaracteresString.kt

---
+++ CaracteresString.kt
@@ -0,0 +1,18 @@
+import java.awt.geom.QuadCurve2D
+
+fun main(args: Array<String>) {
+
+    // Character Char
+
+    var caracter:Char = '1'
+
+
+
+    var caracterUnicode = '\u20DF'
+    println(caracterUnicode)
+
+    // String Cadena de Caracteres
+
+    var cadenaTexto:String = "Hola"
+
+}
