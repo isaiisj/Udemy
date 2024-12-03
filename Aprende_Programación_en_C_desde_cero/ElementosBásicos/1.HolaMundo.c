6d2fa19fecd64879e2044b4364bf029f7c561b7c
Isai <47548422+isaiisj@users.noreply.github.com>
2024-12-03 00:16:35 -0600

    Create 1.HolaMundo.c

---
+++ Aprende_Programación_en_C_desde_cero/ElementosBásicos/1.HolaMundo.c
@@ -0,0 +1,14 @@
+/***********************************************************************
+ 
+    Estructura general de un programa en C
+ 
+***********************************************************************/
+
+#include <stdio.h>
+
+int main() //Funcion principal
+{
+printf("Hola Mundo :D\n");
+
+    return 0;
+}
