ec56b80efca427c7a0e480292fea2e20d6c617b6
Isai <47548422+isaiisj@users.noreply.github.com>
2024-12-14 04:03:28 -0600

    Update Exercise1_BigO(n).java

--- Java_Data_Structures_&_Algorithms_+_LEETCODE_Excercises/Exercise1_BigO(n).java
+++ Java_Data_Structures_&_Algorithms_+_LEETCODE_Excercises/Exercise1_BigO(n).java
@@ -48,6 +48,7 @@ public class Main {
         for(int i = 0; i < n; i++){
             System.out.println(i);
         }
+        
         ////////////////////////////////////////////////////
     }
 
