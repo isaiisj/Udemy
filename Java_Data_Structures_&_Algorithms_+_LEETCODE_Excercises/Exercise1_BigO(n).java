3c49c5e4b9c13489372efc0b43363ffcd3c44772
Isai <47548422+isaiisj@users.noreply.github.com>
2024-12-14 03:45:19 -0600

    Update Exercise1_BigO(n).java

--- Java_Data_Structures_&_Algorithms_+_LEETCODE_Excercises/Exercise1_BigO(n).java
+++ Java_Data_Structures_&_Algorithms_+_LEETCODE_Excercises/Exercise1_BigO(n).java
@@ -39,3 +39,24 @@ For instance, if called with n = 10, the output should be:
 9
 
 */
+
+public class Main {
+
+    public static void printItems(int n) {
+        // WRITE THE FOR LOOP TO COMPLETE PRINTITEMS HERE //
+        //                                                //
+        //                                                //
+        ////////////////////////////////////////////////////
+        for(int i = 0; i < n; i++){
+            System.out.println(i);
+        }
+    }
+
+
+
+    // DO NOT CHANGE THE MAIN METHOD BELOW
+    public static void main(String[] args) {
+        printItems(10);
+    }
+
+}
