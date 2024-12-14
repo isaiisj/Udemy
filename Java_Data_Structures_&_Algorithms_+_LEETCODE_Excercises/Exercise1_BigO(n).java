29f853880fcdd3fe3e0faefda91afaeae9044d4e
Isai <47548422+isaiisj@users.noreply.github.com>
2024-12-14 04:02:29 -0600

    Update Exercise1_BigO(n).java

--- Java_Data_Structures_&_Algorithms_+_LEETCODE_Excercises/Exercise1_BigO(n).java
+++ Java_Data_Structures_&_Algorithms_+_LEETCODE_Excercises/Exercise1_BigO(n).java
@@ -44,11 +44,11 @@ public class Main {
 
     public static void printItems(int n) {
         // WRITE THE FOR LOOP TO COMPLETE PRINTITEMS HERE //
-        //                                                //
-        //                                                //
-        ////////////////////////////////////////////////////
+        
         for(int i = 0; i < n; i++){
             System.out.println(i);
+            
+        ////////////////////////////////////////////////////
         }
     }
 
