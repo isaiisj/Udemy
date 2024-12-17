bf79cf838d908a2289fac95d6df624bb3c640d18
Isai <47548422+isaiisj@users.noreply.github.com>
2024-12-17 00:26:18 -0600

    Update LinkedList.java

--- Java_Data_Structures_&_Algorithms_+_LEETCODE_Excercises/Exercises/Exercise3_LinkedListAppend/LinkedList.java
+++ Java_Data_Structures_&_Algorithms_+_LEETCODE_Excercises/Exercises/Exercise3_LinkedListAppend/LinkedList.java
@@ -7,19 +7,19 @@ Return type: void
 
 The method should perform the following tasks:
 
-1. Accept an integer value as an argument, which will be the value of the new node.
+	1. Accept an integer value as an argument, which will be the value of the new node.
 
-2. Create a new Node object called newNode with the given value.
+	2. Create a new Node object called newNode with the given value.
 
-3. If the length of the linked list is 0, set both the head and tail pointers of the list to the newNode.
+	3. If the length of the linked list is 0, set both the head and tail pointers of the list to the newNode.
 
-4. If the length of the linked list is greater than 0, perform the following tasks:
+	4. If the length of the linked list is greater than 0, perform the following tasks:
 
-	- Set the next attribute of the current tail node to the newNode.
+		Set the next attribute of the current tail node to the newNode.
 
-	- Update the tail pointer of the list to point to the newNode.
+		Update the tail pointer of the list to point to the newNode.
 
-5. Increment the length attribute of the list by 1.
+	5. Increment the length attribute of the list by 1.
 
 */
 
