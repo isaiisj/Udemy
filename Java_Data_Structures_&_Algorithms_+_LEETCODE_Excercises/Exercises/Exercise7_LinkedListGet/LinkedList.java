85e7b4c4c0fcf6f7987d46914908a1aabaf0b258
Isai <47548422+isaiisj@users.noreply.github.com>
2024-12-27 02:34:58 -0600

    Create LinkedList.java

---
+++ Java_Data_Structures_&_Algorithms_+_LEETCODE_Excercises/Exercises/Exercise7_LinkedListGet/LinkedList.java
@@ -0,0 +1,164 @@
+/*
+
+Implement a method called get that retrieves a node from the linked list by its index.
+
+Return type: Node
+
+
+
+The method should perform the following tasks:
+
+    1. Accept an integer index as an argument, which will be the index of 
+       the node to be retrieved.
+
+    2. If the index is less than 0 or greater than or equal to the length of the 
+       linked list, return null.
+
+    3. Create a temporary Node object called temp and set it to the current head node.
+
+    4. Iterate through the linked list by updating temp to its next node until the 
+       desired index is reached.
+
+    5. Return the node located at the specified index.
+
+*/
+
+
+public class LinkedList {
+
+    private Node head;
+    private Node tail;
+    private int length;
+
+    class Node {
+        int value;
+        Node next;
+
+        Node(int value) {
+            this.value = value;
+        }
+    }
+
+    public LinkedList(int value) {
+        Node newNode = new Node(value);
+        head = newNode;
+        tail = newNode;
+        length = 1;
+    }
+
+    public Node getHead() {
+        return head;
+    }
+
+    public Node getTail() {
+        return tail;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void printList() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.println(temp.value);
+            temp = temp.next;
+        }
+    }
+
+    public void printAll() {
+        if (length == 0) {
+            System.out.println("Head: null");
+            System.out.println("Tail: null");
+        } else {
+            System.out.println("Head: " + head.value);
+            System.out.println("Tail: " + tail.value);
+        }
+        System.out.println("Length:" + length);
+        System.out.println("\nLinked List:");
+        if (length == 0) {
+            System.out.println("empty");
+        } else {
+            printList();
+        }
+    }
+    
+    public void makeEmpty() {
+        head = null;
+        tail = null;
+        length = 0;
+    }
+
+    public void append(int value) {
+        Node newNode = new Node(value);
+        if (length == 0) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail.next = newNode;
+            tail = newNode;
+        }
+        length++;
+    }
+
+    public Node removeLast() {
+        if (length == 0) return null;
+        Node temp = head;
+        Node pre = head;
+        while(temp.next != null) {
+            pre = temp;
+            temp = temp.next;
+        }
+        tail = pre;
+        tail.next = null;
+        length--;
+        if (length == 0) {
+            head = null;
+            tail = null;
+        }
+        return temp;
+    }
+
+    public void prepend(int value) {
+        Node newNode = new Node(value);
+        if (length == 0) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            newNode.next = head;
+            head = newNode;
+        }
+        length++;
+    }
+
+    public Node removeFirst() {
+        if (length == 0) return null;
+        Node temp = head;
+        head = head.next;
+        temp.next = null;
+        length--;
+        if (length == 0) {
+            tail = null;
+        }
+        return temp;
+    }
+
+	/// WRITE GET METHOD HERE ///
+
+    public Node get(int index){
+        
+        if(index < 0 || index >= length){
+            return null;
+        }
+        
+        Node temp = head;
+        for(int i = 0; i < index; i++){
+            temp = temp.next;
+        }
+        
+        return temp;
+    }
+    
+	/////////////////////////////
+    
+}
