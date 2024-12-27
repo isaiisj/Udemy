47eaef972d9691f63717c08babeb80b824920b53
Isai <47548422+isaiisj@users.noreply.github.com>
2024-12-27 02:54:40 -0600

    Create LinkedList.java

---
+++ Java_Data_Structures_&_Algorithms_+_LEETCODE_Excercises/Exercises/Exercise8_LinkedListSet/LinkedList.java
@@ -0,0 +1,172 @@
+/*
+
+Implement a method called set that updates the value of a node in the linked list by its index.
+
+Return type: boolean
+
+Method signature: public boolean set(int index, int value)
+
+
+
+The method should perform the following tasks:
+
+    1. Accept an integer index as an argument, representing the index of the node to be updated.
+
+    2. Accept an integer value as an argument, representing the new value to set for the node 
+       at the specified index.
+
+    3. Call the get method with the provided index to obtain a pointer to the node at the specified 
+       index.
+
+    4. If the node exists (i.e., the get method returns a non-null value), update the node's value 
+       with the provided value and return true.
+
+    5. If the node does not exist (i.e., the get method returns null), return false.
+
+*/
+
+
+public class LinkedList {
+
+    private Node head;
+    private Node tail;
+    private int length;
+
+    class Node {
+        int value;
+        Node next;
+
+        Node(int value) {
+            this.value = value;
+        }
+    }
+
+    public LinkedList(int value) {
+        Node newNode = new Node(value);
+        head = newNode;
+        tail = newNode;
+        length = 1;
+    }
+
+    public Node getHead() {
+        return head;
+    }
+
+    public Node getTail() {
+        return tail;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void printList() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.println(temp.value);
+            temp = temp.next;
+        }
+    }
+
+    public void printAll() {
+        if (length == 0) {
+            System.out.println("Head: null");
+            System.out.println("Tail: null");
+        } else {
+            System.out.println("Head: " + head.value);
+            System.out.println("Tail: " + tail.value);
+        }
+        System.out.println("Length:" + length);
+        System.out.println("\nLinked List:");
+        if (length == 0) {
+            System.out.println("empty");
+        } else {
+            printList();
+        }
+    }
+    
+    public void makeEmpty() {
+        head = null;
+        tail = null;
+        length = 0;
+    }
+
+    public void append(int value) {
+        Node newNode = new Node(value);
+        if (length == 0) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail.next = newNode;
+            tail = newNode;
+        }
+        length++;
+    }
+
+    public Node removeLast() {
+        if (length == 0) return null;
+        Node temp = head;
+        Node pre = head;
+        while(temp.next != null) {
+            pre = temp;
+            temp = temp.next;
+        }
+        tail = pre;
+        tail.next = null;
+        length--;
+        if (length == 0) {
+            head = null;
+            tail = null;
+        }
+        return temp;
+    }
+
+    public void prepend(int value) {
+        Node newNode = new Node(value);
+        if (length == 0) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            newNode.next = head;
+            head = newNode;
+        }
+        length++;
+    }
+
+    public Node removeFirst() {
+        if (length == 0) return null;
+        Node temp = head;
+        head = head.next;
+        temp.next = null;
+        length--;
+        if (length == 0) {
+            tail = null;
+        }
+        return temp;
+    }
+
+    public Node get(int index) {
+        if (index < 0 || index >= length) return null;
+        Node temp = head;
+        for(int i = 0; i < index; i++) {
+            temp = temp.next;
+        }
+        return temp;
+    }
+
+	/// WRITE SET METHOD HERE ///
+	
+	public boolean set(int index, int value){
+	    
+	    Node temp = get(index);
+	    if(temp != null){
+	        temp.value = value;
+	        return true;
+	    }
+	    
+	    return false;
+	}
+	
+	/////////////////////////////
+
+}
