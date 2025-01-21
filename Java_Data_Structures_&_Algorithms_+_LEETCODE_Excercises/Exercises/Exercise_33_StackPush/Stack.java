ffee7c05f12262822b25d9da541860a920f186be
Isai <47548422+isaiisj@users.noreply.github.com>
2025-01-21 03:48:39 -0600

    Update Stack.java

--- Java_Data_Structures_&_Algorithms_+_LEETCODE_Excercises/Exercises/Exercise_33_StackPush/Stack.java
+++ Java_Data_Structures_&_Algorithms_+_LEETCODE_Excercises/Exercises/Exercise_33_StackPush/Stack.java
@@ -1,30 +1,3 @@
-/*
-
-Implement a push method that adds a new node with a given value to the top of the stack.
-
-Return type: void
-
-
-
-The method should perform the following tasks:
-
-    1. Accept an integer value as an argument, which will be the value of the new node.
-
-    2. Create a new Node object called newNode with the given value.
-
-    3. If the height of the stack is 0, set the top attribute to the newNode.
-
-    4. If the height of the stack is greater than 0, perform the following tasks:
-
-        Set the next attribute of the newNode to the current top node.
-
-        Update the top attribute to point to the newNode.
-
-    5. Increment the height attribute of the stack by 1.
-
-*/
-
-
 public class Stack {
 
     private Node top;
