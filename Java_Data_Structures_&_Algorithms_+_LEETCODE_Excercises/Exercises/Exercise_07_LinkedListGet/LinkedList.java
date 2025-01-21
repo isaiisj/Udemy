e1dfee84297f132bef45d7722fb3bec6cd165526
Isai <47548422+isaiisj@users.noreply.github.com>
2025-01-21 03:18:21 -0600

    Update LinkedList.java

--- Java_Data_Structures_&_Algorithms_+_LEETCODE_Excercises/Exercises/Exercise_07_LinkedListGet/LinkedList.java
+++ Java_Data_Structures_&_Algorithms_+_LEETCODE_Excercises/Exercises/Exercise_07_LinkedListGet/LinkedList.java
@@ -1,29 +1,3 @@
-/*
-
-Implement a method called get that retrieves a node from the linked list by its index.
-
-Return type: Node
-
-
-
-The method should perform the following tasks:
-
-    1. Accept an integer index as an argument, which will be the index of 
-       the node to be retrieved.
-
-    2. If the index is less than 0 or greater than or equal to the length of the 
-       linked list, return null.
-
-    3. Create a temporary Node object called temp and set it to the current head node.
-
-    4. Iterate through the linked list by updating temp to its next node until the 
-       desired index is reached.
-
-    5. Return the node located at the specified index.
-
-*/
-
-
 public class LinkedList {
 
     private Node head;
