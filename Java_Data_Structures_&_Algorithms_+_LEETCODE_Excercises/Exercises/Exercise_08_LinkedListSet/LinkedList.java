adf18267eb5a5519f6580fcbdfecc00ecac8f1fb
Isai <47548422+isaiisj@users.noreply.github.com>
2025-01-21 03:22:57 -0600

    Update LinkedList.java

--- Java_Data_Structures_&_Algorithms_+_LEETCODE_Excercises/Exercises/Exercise_08_LinkedListSet/LinkedList.java
+++ Java_Data_Structures_&_Algorithms_+_LEETCODE_Excercises/Exercises/Exercise_08_LinkedListSet/LinkedList.java
@@ -1,31 +1,3 @@
-/*
-
-Implement a method called set that updates the value of a node in the linked list by its index.
-
-Return type: boolean
-
-Method signature: public boolean set(int index, int value)
-
-
-
-The method should perform the following tasks:
-
-    1. Accept an integer index as an argument, representing the index of the node to be updated.
-
-    2. Accept an integer value as an argument, representing the new value to set for the node 
-       at the specified index.
-
-    3. Call the get method with the provided index to obtain a pointer to the node at the specified 
-       index.
-
-    4. If the node exists (i.e., the get method returns a non-null value), update the node's value 
-       with the provided value and return true.
-
-    5. If the node does not exist (i.e., the get method returns null), return false.
-
-*/
-
-
 public class LinkedList {
 
     private Node head;
