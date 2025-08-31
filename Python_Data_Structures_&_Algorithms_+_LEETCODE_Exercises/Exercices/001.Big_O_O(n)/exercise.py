bdfbc0db5174d3323956f37cad484de6c7fa0c40
Isai <47548422+isaiisj@users.noreply.github.com>
2025-08-31 00:02:26 -0600

    Create exercise.py

---
+++ Python_Data_Structures_&_Algorithms_+_LEETCODE_Exercises/Exercices/001.Big_O_O(n)/exercise.py
@@ -0,0 +1,14 @@
+## WRITE THE PRINT_ITEMS FUNCTION HERE ##
+
+def print_items(num: int) -> None:
+    
+    for i in range(10):
+        print(i)
+
+#########################################
+  
+        
+      
+        
+# DO NOT CHANGE THIS LINE:
+print_items(10)
