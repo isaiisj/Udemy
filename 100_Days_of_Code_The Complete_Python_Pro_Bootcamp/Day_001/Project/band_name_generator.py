e312dc63518304b3aa15e958f14e00243ccc26b1
Isai <47548422+isaiisj@users.noreply.github.com>
2026-01-11 00:52:42 -0600

    Create band_name_generator.py

---
+++ 100_Days_of_Code_The Complete_Python_Pro_Bootcamp/Day_001/Project/band_name_generator.py
@@ -0,0 +1,14 @@
+'''
+1. Create a greeting for your program.
+2. Ask the user for the city that they grew up in and store it in a variable.
+3. Ask the user for the name of a pet and store it in a variable.
+4. Combine the name of their city and pet and show them their band name.
+'''
+
+print("Hello!!")
+
+city = input("Which city did you grow up in?: ")
+pet = input("Enter the name of your pet: ")
+bandname = city + pet
+
+print("The name of your band is: " + bandname)
