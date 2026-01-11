06deff650670392490a01b79a753da46d8d2807c
Isai <47548422+isaiisj@users.noreply.github.com>
2026-01-11 01:02:28 -0600

    Create solution.py

---
+++ 100_Days_of_Code_The Complete_Python_Pro_Bootcamp/Day_001/Coding_Excercise_02/solution.py
@@ -0,0 +1,51 @@
+'''
+line 1:
+
+print(Notes from Day 1")
+
+Missing starting double quote. Should be:
+
+print("Notes from Day 1")
+
+
+line 2:
+
+print("The print statement is used to output strings")
+
+Has an extra space before the start of the line. Should be:
+
+print("The print statement is used to output strings")
+
+
+line 3:
+
+print("Strings are strings of characters"
+
+Missing the closing parenthesis. Should be: 
+
+print("Strings are strings of characters")  
+
+
+line 4:
+
+priint("String Concatenation is done with the + sign")
+
+Typo in the command "priint" instead of "print". Should be:
+
+print("String Concatenation is done with the + sign")
+
+
+line 5:
+
+print(("New lines can be created with a \ and the letter n")
+
+Extra opening parenthesis. Should be:
+
+print("New lines can be created with a \ and the letter n")
+'''
+
+print("Notes from Day 1")
+print("The print statement is used to output strings")
+print("Strings are strings of characters")
+print("String Concatenation is done with the + sign")
+print("New lines can be created with a \ and the letter n")
