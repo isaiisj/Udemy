7d25ef6e4b17368f3e49afda50ee0f0aa7139ef0
Isai <47548422+isaiisj@users.noreply.github.com>
2026-02-13 02:21:04 -0600

    Create task.py

---
+++ 100_Days_of_Code_The Complete_Python_Pro_Bootcamp/Day_002/03_MathematicalOperations/task.py
@@ -0,0 +1,25 @@
+'''
+Basic Operators
+Learn to use the basic mathematical operators, +, -, *, /, // and **
+
+PEMDAS
+Parentheses, Exponents, Multiplication/Division, Addition/Subtraction
+
+PAUSE 1. What is the output of this code?
+print(3 * 3 + 3 / 3 - 3)
+
+PAUSE 2. Change the code so it outputs 3?
+print(3 * 3 + 3 / 3 - 3)
+'''
+
+print("My age: " + str(12))
+print(123 + 456)
+print(7 - 3)
+print(3 * 2)
+print(6 / 3)
+print(6 // 3)
+print(2 ** 3)
+
+print(3 * 3 + 3 / 3 - 3)
+
+print(3 * (3 + 3) / 3 - 3)
