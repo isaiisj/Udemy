cc09d6a0ff912b2e65d857275993d1d3c5b90723
Isai <47548422+isaiisj@users.noreply.github.com>
2026-02-17 00:29:47 -0600

    Update task.py

--- 100_Days_of_Code_The Complete_Python_Pro_Bootcamp/Day_006/02_TheHurdlesLoopChallenge/task.py
+++ 100_Days_of_Code_The Complete_Python_Pro_Bootcamp/Day_006/02_TheHurdlesLoopChallenge/task.py
@@ -1,3 +1,13 @@
+'''
+Hurdles race
+
+Reeborg has entered a hurdles race. Make him run the course, following the path shown.
+
+What you need to know
+- The functions move() and turn_left().
+'''
+
+
 def turn_right():
     for i in range(3):
         turn_left()
