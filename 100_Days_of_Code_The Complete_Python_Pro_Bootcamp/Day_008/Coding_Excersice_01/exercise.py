7265f20eebb24633f38455d04e6a35e50525f16f
Isai <47548422+isaiisj@users.noreply.github.com>
2026-03-05 18:07:33 -0600

    Create exercise.py

---
+++ 100_Days_of_Code_The Complete_Python_Pro_Bootcamp/Day_008/Coding_Excersice_01/exercise.py
@@ -0,0 +1,6 @@
+def life_in_weeks(age):
+    weeks_to_ninety = 90 * 52
+    years_to_weeks = age * 52
+    weeks_left = weeks_to_ninety - years_to_weeks
+    print(f"You have {weeks_left} weeks left.")
+  
