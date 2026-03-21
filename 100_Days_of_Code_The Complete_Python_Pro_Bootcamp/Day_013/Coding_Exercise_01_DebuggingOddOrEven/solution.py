23205174a53d425b702e987931b388cb5154a66d
Isai <47548422+isaiisj@users.noreply.github.com>
2026-03-21 14:33:50 -0600

    Create solution.py

---
+++ 100_Days_of_Code_The Complete_Python_Pro_Bootcamp/Day_013/Coding_Exercise_01_DebuggingOddOrEven/solution.py
@@ -0,0 +1,6 @@
+def odd_or_even(number):
+    # Bug: It should be == instead of =
+    if number % 2 == 0:
+        return "This is an even number."
+    else:
+        return "This is an odd number."
