0fa98885a10c837d79c03296361d0212ea423dee
Isai <47548422+isaiisj@users.noreply.github.com>
2026-03-28 04:00:16 -0600

    Create main.py

---
+++ 100_Days_of_Code_The Complete_Python_Pro_Bootcamp/Day_033/03_UnderstandAPIParameters/main.py
@@ -0,0 +1,33 @@
+import requests
+from datetime import datetime
+
+MY_LAT = 19.646078
+MY_LONG = -99.211523
+
+# For this API we need required parameters to make it work
+# parameters names must match the names in the documentation
+parameters = {
+    "lat": MY_LAT,
+    "lng": MY_LONG,
+    "formatted": 0,
+    "tzid": "America/Mexico_City"
+}
+
+# we can pass parameters in dictionary form
+response = requests.get("https://api.sunrise-sunset.org/json",params=parameters)
+response.raise_for_status()
+
+data = response.json()
+sunrise = data["results"]["sunrise"].split("T")[1].split(":")[0]
+sunset = data["results"]["sunset"].split("T")[1].split(":")[0]
+
+print(sunrise)
+print(sunset)
+
+time_now = datetime.now()
+
+print(time_now.hour)
+
+
+# sunrise and sunset API: https://sunrise-sunset.org/api
+# Challenge 1: Modify the API call to turn oof the formatting time
