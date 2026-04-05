0646e2fd39abbea729d8193d8339cdf6a98c2a72
Isai <47548422+isaiisj@users.noreply.github.com>
2026-04-05 13:55:00 -0600

    Create main.py

---
+++ 100_Days_of_Code_The Complete_Python_Pro_Bootcamp/Day_032/01_HowToSendEmailsWithPythonUsingSMTP/main.py
@@ -0,0 +1,44 @@
+import smtplib
+
+# Your email here
+my_email = "nota.buss@gmail.com"
+
+#Your app password created in gmail
+password = "hsdc pjmd gbfj voae"
+
+# establish connection with the smtp server
+#connection = smtplib.SMTP("smtp.gmail.com", port=587)
+
+# enable encryption to make the connection secure
+#connection.starttls()
+
+# login to the email using app password
+#connection.login(user=my_email,password=password)
+
+# sender and recipient
+#connection.sendmail(
+#    from_addr=my_email,
+#    to_addrs=my_email,
+#    msg="Subject:Hello\n\nThis is the body of my email"
+#)
+
+# it is necessary if we don't use with ... as ...
+#connection.close()
+
+
+# with ... as ...
+with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
+
+    # enable encryption to make the connection secure
+    connection.starttls()
+
+    # login to the email using app password
+    connection.login(user=my_email,password=password)
+
+    # sender and recipient
+    connection.sendmail(
+        from_addr=my_email,
+        to_addrs=my_email,
+        msg="Subject:Hello\n\nThis is the body of my email"
+    )
+  
